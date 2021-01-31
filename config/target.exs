18d2e4c2f17e605152c3b371ed37d688a1125e36
Mattias Lundberg <me@mlundberg.se>
2021-01-31 15:33:57 +0100

    Configure networking

--- config/target.exs
+++ config/target.exs
@@ -52,7 +52,7 @@ config :nerves_ssh,
 # Configure the network using vintage_net
 # See https://github.com/nerves-networking/vintage_net for more information
 config :vintage_net,
-  regulatory_domain: "US",
+  regulatory_domain: "EU",
   config: [
     {"usb0", %{type: VintageNetDirect}},
     {"eth0",
@@ -60,7 +60,20 @@ config :vintage_net,
        type: VintageNetEthernet,
        ipv4: %{method: :dhcp}
      }},
-    {"wlan0", %{type: VintageNetWiFi}}
+    {"wlan0",
+     %{
+       type: VintageNetWiFi,
+       vintage_net_wifi: %{
+         networks: [
+           %{
+             key_mgmt: :wpa_psk,
+             ssid: System.fetch_env!("HOME_DISPLAY_WIFI_SSID"),
+             psk: System.fetch_env!("HOME_DISPLAY_WIFI_PSK")
+           }
+         ]
+       },
+       ipv4: %{method: :dhcp}
+     }}
   ]
 
 config :mdns_lite,
