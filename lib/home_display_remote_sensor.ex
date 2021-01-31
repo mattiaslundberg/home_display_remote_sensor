671c6239bef2687da053a1fe8ac8d3e12893946a
Mattias Lundberg <me@mlundberg.se>
2021-01-31 14:13:30 +0100

    Generated project shell

---
+++ lib/home_display_remote_sensor.ex
@@ -0,0 +1,18 @@
+defmodule HomeDisplayRemoteSensor do
+  @moduledoc """
+  Documentation for HomeDisplayRemoteSensor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HomeDisplayRemoteSensor.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/home_display_remote_sensor_test.exs
@@ -0,0 +1,8 @@
+defmodule HomeDisplayRemoteSensorTest do
+  use ExUnit.Case
+  doctest HomeDisplayRemoteSensor
+
+  test "greets the world" do
+    assert HomeDisplayRemoteSensor.hello() == :world
+  end
+end
